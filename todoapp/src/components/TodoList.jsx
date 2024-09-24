e6742292b67e2b0cd7e821056e9c0c3be005c5c4
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-09-25 04:21:52 +0900

    feat: develop complexity of the app to sort todo list based on the finished or unfinished

--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -3,9 +3,13 @@ import TodoItem from "./TodoItem";
 import "../css/todoitem.css";
 
 function TodoList({ todos, setTodos }) {
+  // slice creates a copy of an array
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className="task-list">
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItem
           item={item}
           key={item.name}
